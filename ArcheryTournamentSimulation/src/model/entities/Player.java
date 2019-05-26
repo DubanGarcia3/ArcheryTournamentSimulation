10df13d473abee5fe34bcbf1d7e78330947bdb1e
Duban <mr.garcialml@gmail.com>
2019-05-26 10:50:19 -0500

    createmodelpackage

---
+++ ArcheryTournamentSimulation/src/model/entities/Player.java
@@ -0,0 +1,12 @@
+package model.entities;
+
+public class Player {
+	
+	private Gender gender;
+	private int age;
+	private int resistence;
+	private int aim;
+	private int exp;
+	private int lucky;
+	
+}

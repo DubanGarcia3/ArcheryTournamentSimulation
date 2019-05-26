83d11444d66e07878e57443b78dc2a47cf3eaf26
Mille <Mille@DESKTOP-0BE4567>
2019-05-26 11:37:46 -0500

    sadasdas

--- ArcheryTournamentSimulation/src/model/entities/Team.java
+++ ArcheryTournamentSimulation/src/model/entities/Team.java
@@ -1,6 +1,20 @@
 package model.entities;
 
+import java.util.ArrayList;
+
 public class Team {
 	
+	private ArrayList<Player> playerList;
+	
+	public Team() {
+		playerList = new ArrayList<Player>();
+	}
 
+	public ArrayList<Player> getPlayerList() {
+		return playerList;
+	}
+
+	public void setPlayerList(ArrayList<Player> playerList) {
+		this.playerList = playerList;
+	}
 }

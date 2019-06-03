7a6f0b737253c12752b8c322bdb8bbc378ebac71
Duban <mr.garcialml@gmail.com>
2019-06-03 12:30:48 -0500

    agregar en memoria los lanzamientos

--- ArcheryTournamentSimulation/src/dao/Manager/ManagerShot.java
+++ ArcheryTournamentSimulation/src/dao/Manager/ManagerShot.java
@@ -1,5 +1,33 @@
 package dao.Manager;
 
-public class ManagerShot {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import model.entities.Shot;
+import persistence.FileManager;
+
+public class ManagerShot {
+	
+	private List<Shot> listshot;
+
+	public ManagerShot(List<Shot> listshot) {
+		this.listshot = new ArrayList<Shot>();
+	}
+	
+	public void addFromPersistence() {
+		try {
+			for (int i = 0; i < FileManager.readFileLanzamientos().size(); i++) {
+				if (FileManager.readFileLanzamientos() != null) {
+					listshot.add(this.createPlayer(FileManager.splitLine(FileManager.readFileLanzamientos().get(i), ",")));
+				}
+			}
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+	}
+
+	public Shot createPlayer(String []in) {
+		return new Shot(Double.parseDouble(in[0]), Double.parseDouble(in[1]));
+	}
 }

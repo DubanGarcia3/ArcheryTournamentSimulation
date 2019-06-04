7c25018062cfca3eb53a0830948434b9e505a7b8
Duban <mr.garcialml@gmail.com>
2019-06-03 21:28:10 -0500

    miller puto

--- ArcheryTournamentSimulation/src/model/entities/Team.java
+++ ArcheryTournamentSimulation/src/model/entities/Team.java
@@ -9,16 +9,14 @@ public class Team {
 	private ArrayList<Player> playerList;
 	
 	public Team() {
-		this.id = ID_BASE++;
 		playerList = new ArrayList<Player>();
+		this.id = ID_BASE++;
 	}
 	
 	public double calculateTotalDistance() {
 		double aux=0;
 		for (int i = 0; i < playerList.size(); i++) {
-			for (int j = 0; j < playerList.get(i).getListShots().size(); j++) {
-				aux+=playerList.get(i).getListShots().get(j).calcualteDistance();
-			}
+				aux += playerList.get(i).getTotalDistance();
 		}
 		return aux;
 	}
--- ArcheryTournamentSimulation/src/run/Runner.java
+++ ArcheryTournamentSimulation/src/run/Runner.java
@@ -9,5 +9,4 @@ public class Runner {
 		controller.loadData();
 		controller.createMatch();
 	}
-
 }

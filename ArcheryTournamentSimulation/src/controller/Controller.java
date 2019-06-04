76b2f1ae9ab4b15abe83c3724bcebdc604e70c08
Duban <mr.garcialml@gmail.com>
2019-06-03 23:01:25 -0500

    ora si

--- ArcheryTournamentSimulation/src/controller/Controller.java
+++ ArcheryTournamentSimulation/src/controller/Controller.java
@@ -119,9 +119,9 @@ public class Controller {
 
 	public void manageFile() {
 		try {
-			List<String> x = FileManager.readFile();
-			for (int i = 0; i < FileManager.readFile().size(); i++) {
-				listData.add(this.createPlayer(FileManager.splitLine(FileManager.readFile().get(i), ",")));
+			List<String> file = FileManager.readFile();
+			for (int i = 0; i < file.size(); i++) {
+				listData.add(this.createPlayer(FileManager.splitLine(file.get(i), ",")));
 			}
 		} catch (IOException e) {
 			System.out.println(e);
